a04f44567d5241ed2ef96fa3bece052860cab159
1Jayso <adodey1@yahoo.com>
2021-01-17 12:18:21 +0000

    Added TextField to the join page

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom"
 
 export default class RoomJoinPage extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            roomCode: "",
+            error: "hello"
+        }
     }
 
     
     render(){
-        return <p> This is the Join room page </p>
+        return (
+            <Grid container spacing={1} alignItems="center">
+                <Grid item xs={12}>
+                    <Typography variant="h4 component="h4>
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        error={this.state.error}
+                        label="code"
+                        placeholder="Enter a Room Code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                        />
+                </Grid>
+            </Grid>
+        )
     }
 
 } 

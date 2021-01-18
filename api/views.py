42de65ab84632d7631af2f8d176fe67ebb46b928
1Jayso <adodey1@yahoo.com>
2021-01-18 00:02:20 +0000

    Added a class RooomJoin

--- api/views.py
+++ api/views.py
@@ -52,10 +52,6 @@ class JoinRoom(APIView):
 
 
 
-
-
-
-
 # Creating Room
 class CreateRoomView(APIView):
     # queryset = Room.objects.all()
@@ -78,11 +74,13 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
+                self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)

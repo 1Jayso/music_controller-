d087e9a59cd623ace6bc8943fdb1cd8fa031cd50
1Jayso <adodey1@yahoo.com>
2021-01-31 01:32:46 +0000

    added buttons  to create and join a room in the homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import RoomJoinPage from './RoomJoinPage'
 import CreateRoom from './CreateRoomPage'
 import Room from './Room'
+import {Grid, Button, ButtonGroup, Typography} from '@material-ui/core'
 import { BrowserRouter as Router, Route, Switch,Link,Redirect } from 'react-router-dom';
 
 
@@ -11,11 +12,35 @@ export default class HomePage extends Component{
     }
 
 
+    renderHomePage(){
+        return(
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3"cpmpact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to='/join' component={Link}>
+                            Join a Room
+                        </Button>
+
+                        <Button color="secondary" to='/create' component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+
+    }
+
     render(){
         return(
             <Router>
                 <Switch>
-                    <Route exact path='/' > <p> This is the home page </p></Route>
+                    <Route exact path='/'> {this.renderHomePage()}</Route>
                     <Route path='/join' component={RoomJoinPage}/>
                     <Route  path='/create' component={CreateRoom} />
                     <Route path="/room/:roomCode" component={Room} />
